706f6a9a5674193814ed3ac92d63d273fb56d5d0
Sk Mijanur Rahaman <mijansk1994@gmail.com>
2024-08-20 12:58:10 +0530

    Commit upto File Reader

--- ReadData/ReadDataDemo1.java
+++ ReadData/ReadDataDemo1.java
@@ -11,7 +11,11 @@ public class ReadDataDemo1 {
         //Step 1 : Create an object of File Input Stream Class which is already in JAVA
         //Instead of file reader we can user File Input Stream
         //Step 2: Create an object of properties class
-        FileInputStream fis = new FileInputStream("D:\\##1Mijanur_Rahaman\\Java_Testing\\Properties_File\\TestData.properties");
+        //FileInputStream fis = new FileInputStream("D:\\##1Mijanur_Rahaman\\Java_Testing\\Properties_File\\TestData.properties");
+
+        //instead of hard coding we can use method to read the file
+        FileInputStream fis = new FileInputStream(System.getProperty("user.dir")+"\\Properties_File\\TestData.properties");
+
         Properties pr = new Properties();
 
         //Step 3: Load the properties file
@@ -20,6 +24,7 @@ public class ReadDataDemo1 {
         //Step 4: Use get property method to get property
         System.out.println(pr.getProperty("Website"));
         System.out.println(pr.getProperty("Name"));
+        //System.out.println(System.getProperty("user.dir"));
     }
 }
 
