15e54c2c6d26cfd99f531db6170b58860d99e4fc
Sk Mijanur Rahaman <mijansk1994@gmail.com>
2024-08-20 12:44:51 +0530

    Upto File Reader using input Stream

--- ReadData/ReadDataDemo1.java
+++ ReadData/ReadDataDemo1.java
@@ -1,24 +1,25 @@
 package ReadData;
 
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.Properties;
 
-public class ReadDataDemo {
-
+public class ReadDataDemo1 {
     public static void main(String[] args) throws IOException {
 
-        //Step 1 : Create an object of FileReader Class which is already in JAVA
-        FileReader fr = new FileReader("D:\\##1Mijanur_Rahaman\\Java_Testing\\Properties_File\\TestData.properties");
-
+        //Step 1 : Create an object of File Input Stream Class which is already in JAVA
+        //Instead of file reader we can user File Input Stream
         //Step 2: Create an object of properties class
+        FileInputStream fis = new FileInputStream("D:\\##1Mijanur_Rahaman\\Java_Testing\\Properties_File\\TestData.properties");
         Properties pr = new Properties();
 
         //Step 3: Load the properties file
-        pr.load(fr);
+        pr.load(fis);
 
         //Step 4: Use get property method to get property
         System.out.println(pr.getProperty("Website"));
         System.out.println(pr.getProperty("Name"));
     }
 }
+
